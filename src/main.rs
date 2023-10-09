eab7b2da86690be680b526fde4b3de7a2e326daa
Peter Roehlen <peter.roehlen@gmail.com>
2023-10-10 00:34:59 +0700

    Fix for error in book

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,19 @@
 use std::sync::Mutex;
-use std::thread;
+use std::thread::scope;
 
 fn main() {
     let counter = Mutex::new(0);
-    let mut handles = vec![];
 
-    for _ in 0..10 {
-        let handle = thread::spawn(move || {
-            let mut num = counter.lock().unwrap();
+    _ = scope(|s|{
+        for _ in 0..10 {
+            s.spawn(|| {
+                let mut num = counter.lock().unwrap();
 
-            *num += 1;
-        });
-        handles.push(handle);
-    }
+                *num += 1;
+            });
+        }
+    });
 
-    for handle in handles {
-        handle.join().unwrap();
-    }
 
     println!("Result: {}", *counter.lock().unwrap());
 }
